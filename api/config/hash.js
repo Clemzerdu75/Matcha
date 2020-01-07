5f621c554e9af5bd29f89f8d65ce9ec2a36dedda
Clement FAUVELLE <cfauvell@e1r5p15.42.fr>
2020-01-07 16:12:06 +0100

    Deleting all

--- api/config/hash.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-module.exports.hash = (password) => {
-  const hash = crypto.createHash('whirlpool');
-  const data = hash.update(password, 'utf-8');
-  const hashedPassword = data.digest('hex');
-  return (hashedPassword);
-};
--- api/routes/users/picture/parse_gallery.js
+++
@@ -1,1 +0,0 @@
-module.exports = ((gallery) => gallery.split(' ').filter((elem) => elem !== ''));
